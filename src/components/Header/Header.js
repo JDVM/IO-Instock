e3e02b4d79fc0e7f9977a837c676c79f117a54b6
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-15 23:29:22 -0400

    finished styling and linking of header component. Also added to browser router so it shows up in every page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,36 @@
-import { Link } from "react-router-dom"
-import logo from "../../assets/images/Logo/InStock-Logo_2x.png"
-import "./Header.scss"
-
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/images/Logo/InStock-Logo_2x.png";
+import "./Header.scss";
 
 function Header() {
-    return(
-        <header className="header">
-            <img className="header__logo" src={logo} alt="Logo for Instock Company"/>
-            <nav className="header__nav">
-                <Link className="header__warehouses header__active" to={`/Warehouses`}><div>Warehouses</div></Link>
-                <Link className="header__inventory" to={`/Inventory`}><div>Inventory</div></Link>
-            </nav>
-        </header>
-    )    
+  const location = useLocation();
+
+  const isActiveLink = (path) => location.pathname === path;
+
+  return (
+    <header className="header">
+      <Link to={"/Warehouses"}><img className="header__logo" src={logo} alt="Logo for Instock Company" /></Link>
+      <nav className="header__nav">
+        <Link
+          className={`header__warehouses ${
+            isActiveLink("/Warehouses") ? "header__active" : ""
+          }`}
+          to={"/Warehouses"}
+        >
+          <div>Warehouses</div>
+        </Link>
+        <Link
+          className={`header__inventory ${
+            isActiveLink("/Inventory") ? "header__active" : ""
+          }`}
+          to={"/Inventory"}
+        >
+          {" "}
+          <div>Inventory</div>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
