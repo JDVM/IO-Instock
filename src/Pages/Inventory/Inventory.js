97b4c36f99db351489dd09a397664ec03791524f
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-19 10:18:10 -0400

    commiting before cloning repo on laptop

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -3,9 +3,6 @@ import "./Inventory.scss";
 
 import AddInventory from "../../components/AddInventory/AddInventory";
 import InventoryDetails from "../../components/InventoryDetails/InventoryDetails";
-import { useLocation } from "react-router-dom";
-import "./Inventory.scss";
-import AddInventory from "../../components/AddInventory/AddInventory";
 
 function Inventory() {
   const location = useLocation();
