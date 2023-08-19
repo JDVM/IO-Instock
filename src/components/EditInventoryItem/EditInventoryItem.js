dc4707140f7c1c9a823b7530ab1ce8d38f45e854
John Carlo Pierce <johncarlopierce@gmail.com>
2023-08-19 10:23:24 -0400

    add states, styles

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -1,51 +1,149 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import arrowBackIcon from '../../assets/images/Icons/arrow_back-24px.svg';
+import arrowBackIcon from "../../assets/images/Icons/arrow_back-24px.svg";
+import './EditinventoryItem.scss';
 
 function EditInventoryItem() {
-    const [itemName, setItemName] = useState("");
-    const [description, setDescription] = useState("");
+  const { itemId } = useParams();
+  const navigate = useNavigate();
 
-    const handleChangeItemName = (e) => {
-        setItemName(e.target.value);
-    };
-    const handleChangeDescription = (e) => {
-        setDescription(e.target.value);
+  const [itemName, setItemName] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedWarehouse, setSelectedWarehouse] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`inventory/${itemId}`);
+        console.log(response)
+        const itemData = response.data;
+
+        setItemName(itemData.item_name);
+        setDescription(itemData.description);
+        setSelectedCategory(itemData.category);
+        setSelectedWarehouse(itemData.warehouse_id);
+      } catch (error) {
+        console.error("Error getting item data:", error);
+      }
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="EditInventoryItem__head">
-                <img src={arrowBackIcon} />
-                <h1>Edit Inventory Item</h1>
-            </div>
-            <div className='EditInventoryItem__details'>
-                <h2>Item Details</h2>
-                    <label>
-                        Item Name <input
-                                        type='text'
-                                        name='name'
-                                        onChange={handleChangeItemName}
-                                        value={itemName}/>
-                    </label>
-                    <label>
-                        Description <textarea
-                                        type='text'
-                                        name='description'
-                                        onChange={handleChangeDescription}
-                                        value={description} />
-                    </label>
-                    <h3>Category</h3>
-            </div>
-            <div className='EditInventoryItem__availability'>
-                <h2>Item Availability</h2>
-            </div>
-        </form>
-    );
-};
+    getData();
+  }, [itemId]);
+
+  const handleChangeItemName = (e) => {
+    setItemName(e.target.value);
+  };
+
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const handleWarehouseChange = (e) => {
+    setSelectedWarehouse(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const updatedItem = {
+        item_name: itemName,
+        description: description,
+        category: selectedCategory,
+        warehouse_id: selectedWarehouse,
+      };
+
+      await axios.put(`/api/inventory/${itemId}`, updatedItem);
+      navigate("/inventory");
+    } catch (error) {
+      console.error("Error updating item:", error);
+    }
+  };
+
+  const categoryOptions = [
+    { value: "electronics", label: "Electronics" },
+    { value: "gear", label: "Gear" },
+    { value: "apparel", label: "Apparel" },
+    { value: "accessories", label: "Accessories" },
+    { value: "health", label: "Health" },
+  ];
+
+  const warehouseOptions = [
+    { value: "Manhattan", label: "Manhattan" },
+    { value: "Washington", label: "Washington" },
+    { value: "Jersy", label: "Jersey" },
+    { value: "SF", label: "SF" },
+    { value: "Santa Monica", label: "Santa Monica" },
+    { value: "Seattle", label: "Seattle" },
+    { value: "Miami", label: "Miami" },
+    { value: "Boston", label: "Boston" },
+  ];
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="edit-inventory-item__head">
+        <img className="edit-inventory-item__icon" src={arrowBackIcon} />
+        <h1 className="edit-inventory-item__head-title">Edit Inventory Item</h1>
+      </div>
+      <section className="edit-inventory-item__section">
+        <h2 className="edit-inventory-item__subhead-title">Item Details</h2>
+        <label className="edit-inventory-item__label">
+          Item Name{" "}
+          <input className="edit-inventory-item__input"
+            type="text"
+            name="name"
+            onChange={handleChangeItemName}
+            value={itemName}
+          />
+        </label>
+        <label className="edit-inventory-item__label">
+          Description{" "}
+          <textarea className="edit-inventory-item__input"
+            type="text"
+            name="description"
+            onChange={handleChangeDescription}
+            value={description}
+          />
+        </label>
+        <h3 className="edit-inventory-item__label">Category</h3>
+        <select className="edit-inventory-item__input"
+          name="category"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="">Select a category</option>
+          {categoryOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </section >
+      <section className="edit-inventory-item__section">
+        <h2 className="edit-inventory-item__subhead-title">Item Availability</h2>
+        <h3 className="edit-inventory-item__label">Warehouse</h3>
+        <select className="edit-inventory-item__input"
+          name="warehouse"
+          value={selectedWarehouse}
+          onChange={handleWarehouseChange}
+        >
+          <option value="">Select a warehouse</option>
+          {warehouseOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </section>
+    </form>
+  );
+}
 
 export default EditInventoryItem;
