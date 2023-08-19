0f7c1b55a307b3e53ee5cfacc96c17b9463afa71
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-08-19 11:35:39 -0400

    fix warehouses.js bug

--- src/utils/getWarehouseById.js
+++ src/utils/getWarehouseById.js
@@ -4,8 +4,10 @@ const getWarehouseById = async (id) => {
   const BASE_URL = process.env.REACT_APP_API_URL;
   const API_PORT = process.env.REACT_APP_API_PORT;
   const API_KEY = process.env.REACT_APP_API_KEY;
-  const API_URL = `${BASE_URL}:${API_PORT}/warehouses/${id}${API_KEY ? `?api_key=${API_KEY}`  : ""}`;
-
+  const API_URL = `${BASE_URL}:${API_PORT}/warehouses/${id}${
+    API_KEY ? `?api_key=${API_KEY}` : ""
+  }`;
+  console.log(API_URL);
   return axios
     .get(API_URL)
     .then((res) => {
