537e0186079f71038f3a1468a861100d5cdbfd72
Juan Velilla <juandvelillamantilla@gmail.com>
2023-08-21 00:43:05 -0400

    added pages folder correctly

---
+++ src/pages/Inventory/Inventory.js
@@ -0,0 +1,48 @@
+import { useLocation, useParams } from "react-router-dom";
+import "./Inventory.scss";
+import AddInventory from "../../components/AddInventory/AddInventory";
+
+import EditInventoryItem from "../../components/EditInventoryItem/EditInventoryItem";
+
+import InventoryDetails from "../../components/InventoryDetails/InventoryDetails";
+import InventoryList from "../../components/InventoryList/InventoryList";
+
+
+function Inventory() {
+  const location = useLocation();
+  const { id } = useParams();
+  console.log(location.pathname);
+  if (location.pathname.endsWith("/new"))
+    return (
+      <div className="inventories">
+        <AddInventory />
+      </div>
+    );
+
+
+  if (location.pathname.endsWith("/edit"))
+    return (
+      <div className="inventories">
+        <EditInventoryItem />
+      </div>
+    );
+
+  if (location.pathname.endsWith(`/${id}`))
+    return (
+      <div>
+        <InventoryDetails />
+      </div>
+    )
+
+
+
+
+
+  return (
+    <div className="inventories">
+      <InventoryList />
+    </div>
+  );
+}
+
+export default Inventory;
---
+++ src/pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PageNotFound.scss';
+
+const PageNotFound = () => {
+    return (
+        <div className="not-found">
+            <div className="not-found__circle"></div>
+            <h1 className="not-found__title">404</h1>
+            <p className="not-found__text">Page Not Found</p>
+            <p className="not-found__subtext">The page you're looking for doesn't exist or has been moved.</p>
+        </div>
+    );
+}
+
+export default PageNotFound;
